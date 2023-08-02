d229d01a769f8e71df62246a6098b16e87b4923b
Harsh <123166741+haarrrsssshhhhh@users.noreply.github.com>
2023-08-02 16:01:56 +0530

    Update Day_1.sh

--- Solutions/Day_1.sh
+++ Solutions/Day_1.sh
@@ -1,32 +1,62 @@
 #!/bin/bash
 
-# Above line is called shebang which tells the system this is the script and use bash interpretor
+#Author : Harsh Roy
+#Date   : 1-Aug-23
+#Repo   : haarrrsssshhhhh
+#purpose : 7 days bash script challange ---Day1 (Train with Shubham)
 
-# Task  2: Echo
-echo "BashBlaze Challenges..! initited by @TWS"
+#Task-1 comments
 
-# Task  3: Variables
-name="Rishi"
-surname="Ghate"
-giturl="https://github.com/Ghaterishi"
-random_number=$(shuf -i 100000-999999 -n 1)  #create 6 digit random number
+#This script shows the use of comments in a Bash script.
+#Comments start with the `#` symbol and are used to add explanatory notes.
 
-# Task  4: Using Variables
-introduction="Hello I am, $name $surname. If you like my scripts check out my git 
-              profile $giturl for more amazing scripts."
-echo "$introduction"
+#Print a message to the terminal
 
-# Task  5: Using Built-in Variables
-echo "My current bash path - $BASH"
-echo "I am Using - $OSTYPE"
-sudo timedatectl set-timezone Asia/Kolkata  #setting up TimeZone 
-export TZ=Asia/Kolkata                      #export that TimeZone into Variable(not built-in)
-echo "My current TimeZone is- $TZ"
-echo "I can Guess you bank otp - $random_number" #its my superpower
+echo "Hello, My name is Harsh Roy!"
 
-# Task  6: Wildcards
-echo "lets create 10 files using single command.!"
-mkdir -p BashBlaze/Day{1..10}.txt
+#this line is a comment and will be ignored when the script is executed.
 
-#Dont forget to Give your script executable permission
-#chmod +x day1_script.sh
+#Task-2 echo
+
+#This script uses the `echo` command to print a custom message.
+
+echo "Welcome to train with shubham batch 4"
+
+#Task-3 Variables
+
+#This script shows how to declare and assign values to variables in bash.
+#Declare variables
+
+name="Puja"
+age=26
+
+#Print the values of the variables
+echo "Name: $name"
+echo "Age: $age"
+
+#Task-4 Using variables 
+
+#This script takes 2 variables as input and prints their sum.
+
+read -p "Enter the first number: " num1
+read -p "Enter the second number: " num2
+sum=$((num1 + num2))
+
+#print the result
+echo "The sum of $num1 and $num2 is: $sum"
+
+
+#Task-5 Using built-in variables
+
+#This script utilizes built-in variables to display relevant information.
+
+echo "My current bash path: $BASH"
+echo "Bash version i am using: $BASH_Version"
+echo "PID of bash I am running: $$"
+echo "Home directory of current user: $HOME"
+echo "Present working directory of current user: $PWD"
+
+#This script uses wildcards to list all files with a specific extension in a directory.
+
+echo "searching files with .sh extenion "
+ls .sh
